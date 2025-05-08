d5897ece3a6903c01f3941d1baf2f2f1c6081eeb
NarendranAbTR <138242746+NarendranAbTR@users.noreply.github.com>
2025-05-08 23:17:21 +0530

    Delete devCode directory

--- devCode/__init__.py
+++
@@ -1,8 +0,0 @@
-# C:\NETSCAN_Formator\devCode\__init__.py
-import os
-import sys
-
-# Add project root to path
-project_root = os.path.dirname(os.path.dirname(__file__))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
